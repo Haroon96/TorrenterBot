1eb3deb8d8bc759c51883e406d84e4d08e5debb0
Muhammad Haroon <m_haroon96@hotmail.com>
2023-06-26 20:58:25 -0400

    added args for chromedriver

--- torrent_handler.py
+++ torrent_handler.py
@@ -100,7 +100,9 @@ class TorrentHandler:
 
     def search(self, query):
         options = ChromeOptions()
-        options.add_argument('--headless')
+        options.add_argument("--no-sandbox")
+        options.add_argument("--headless")
+        options.add_argument("--disable-gpu")
         driver = Chrome(options=options)
 
         # get website

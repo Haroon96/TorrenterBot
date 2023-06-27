21718b4f72f2c89ffa2e0bde9b1f156ff66e1b7c
Muhammad Haroon <m_haroon96@hotmail.com>
2023-06-27 16:52:16 -0400

    fixed format

--- torrent_handler.py
+++ torrent_handler.py
@@ -65,7 +65,7 @@ class TorrentHandler:
                     continue
 
                 # show results and ask for input
-                message = '\n'.join(['<strong>%s.</strong> %s\n%s\n' % (i, r.name, r.stats) for i, r in enumerate(self.results)])
+                message = '\n'.join(['<strong>%s:</strong> %s\n%s\n' % (i + 1, r.name, r.stats) for i, r in enumerate(self.results)])
                 self.send_message(message, self.build_markup(self.results, index=True))
                 self.state = 'pending_response_1'
             

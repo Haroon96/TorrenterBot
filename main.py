1bbf0c7f41e56fc7bb76b5708d88aa32ac9187de
Muhammad Haroon <m_haroon96@hotmail.com>
2023-06-26 22:55:19 -0400

    fixed indenting

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ from rss_server import start_server
 
 
 def main():
-	# load configuration
-	with open('config.json') as f:
-		config = json.load(f)
+    # load configuration
+    with open('config.json') as f:
+        config = json.load(f)
 
-	# start rss server
-	Thread(target=start_server, args=(config["PORT"],)).start()
+    # start rss server
+    Thread(target=start_server, args=(config["PORT"],)).start()
 
-	# start bot
-	bot = TelegramBot(config["TOKEN"], config["RSS_FEEDS"], f'http://localhost:{config["PORT"]}')
-	bot.start()
+    # start bot
+    bot = TelegramBot(config["TOKEN"], config["RSS_FEEDS"], f'http://localhost:{config["PORT"]}')
+    bot.start()
 
 if __name__ == '__main__':
-	main()
+    main()

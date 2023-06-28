36aefc60be0aa9f8de7189c7636535ea09dbfaf8
Muhammad Haroon <m_haroon96@hotmail.com>
2023-06-27 21:51:22 -0400

    added realpath for all files

--- hook.py
+++ hook.py
@@ -3,14 +3,17 @@ import telebot
 import os
 import json
 
+def get_realpath(file):
+    return os.path.join(os.path.realpath(os.path.dirname(__file__)), file)
+
 def main(torrent_id):
     # get chat id for this torrent id
-    with open('torrent_user_map.csv') as f:
+    with open(get_realpath('torrent_user_map.csv')) as f:
         mapping = f.read().split('\n')
     torrent_id, chat_id, name  = [row for row in mapping if row.startswith(torrent_id.lower())][0].split(':')
 
     # read token from config
-    with open(os.path.join(os.path.realpath(os.path.dirname(__file__)), 'config.json')) as f:
+    with open(get_realpath('config.json')) as f:
         config = json.load(f)
 
     # create bot

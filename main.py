99eabc7e43819b11e020a8fd4d5a637b8fe39e17
Muhammad Haroon <m_haroon96@hotmail.com>
2025-06-14 12:46:23 +0500

    code cleanup + switched to f-strings + added cleanup for finished handlers

--- main.py
+++ main.py
@@ -5,16 +5,17 @@ from threading import Thread
 import requests
 from time import sleep
 import rss_server
+import typing
 
 class TelegramBot:
     def __init__(self, token, rss_feeds, rss_api, num_results, allowed_chat_ids, qbittorrent_credentials):    
         self.bot = telebot.TeleBot(token)
-        self.rss_feeds = rss_feeds
-        self.rss_api = rss_api
-        self.num_results = num_results
-        self.allowed_chat_ids = allowed_chat_ids
-        self.qbittorrent_credentials = qbittorrent_credentials
-        self.handlers = {}
+        self.rss_feeds: typing.List[str] = rss_feeds
+        self.rss_api: str = rss_api
+        self.num_results: int = num_results
+        self.allowed_chat_ids: typing.List[int] = allowed_chat_ids
+        self.qbittorrent_credentials: typing.Dict[str, str] = qbittorrent_credentials
+        self.handlers: typing.Dict[str, MessageHandler] = {}
 
     def bot_message_handler(self, messages: list[telebot.types.Message]):
         for message in messages:
@@ -34,7 +35,9 @@ class TelegramBot:
                     'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
                 }
                 with requests.Session() as session:
-                    data = 'username=%s&password=%s' % (self.qbittorrent_credentials['USERNAME'], self.qbittorrent_credentials['PASSWORD'])
+                    username = self.qbittorrent_credentials['USERNAME']
+                    password = self.qbittorrent_credentials['PASSWORD']
+                    data = f'username={username}&password={password}'
                     session.post(f'{api_url}/auth/login', data=data, headers=headers)
                     req = session.get(f'{api_url}/sync/maindata?rid=0')
                     response = req.json()
@@ -51,7 +54,7 @@ class TelegramBot:
             if message.text.startswith('/torrent'):
                 # check if already in a session with this user and wrap that up
                 if from_user in self.handlers and not self.handlers[from_user].is_finished():
-                    self.handlers[from_user].finished = True
+                    self.handlers[from_user].state = MessageHandler.State.FINISHED
                 self.handlers[from_user] = MessageHandler(self.bot, chat_id, self.rss_api, self.rss_feeds, self.num_results)
                 self.handlers[from_user].start()
 
@@ -61,16 +64,30 @@ class TelegramBot:
 
     def start(self):
         update_id = None
+
+        # loop infinitely
         while True:
             try:
+                # clear out finished handlers
+                keys = list(self.handlers.keys())
+                for key in keys:
+                    handler = self.handlers[key]
+                    if handler.is_finished():
+                        del self.handlers[key]
+
+                # check for new messages
                 updates = self.bot.get_updates(offset=update_id)
-                if not updates:
-                    continue
-                update_id = updates[-1].update_id + 1
-                self.bot_message_handler([upd.message for upd in updates])
+
+                # new updates, process accordingly
+                if updates:
+                    # get latest update_id
+                    update_id = updates[-1].update_id + 1
+                    
+                    # forward to bot handler
+                    self.bot_message_handler([upd.message for upd in updates])
             except KeyboardInterrupt:
                 break
-            except Exception as e:
+            except Exception:
                 sleep(5)
                 continue
 
